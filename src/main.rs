c67b1e0d195937c80909b65a578a4afd1713b0b5
edwin <edwin.abel.3@gmail.com>
2022-12-09 17:51:09 +0800

    little finish.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 mod pig_latin;
+use std::env;
+
 use crate::pig_latin::to_pig_latin;
 
 fn main() {
+    let mut args: Vec<String> = env::args().collect();
+    args.remove(0);
+    args.iter()
+        .map(|f| to_pig_latin(f))
+        .for_each(|f| println!("{} ", f));
     println!("{}", to_pig_latin("apple"));
     println!("{}", to_pig_latin("first"));
 }
