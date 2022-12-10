44c2742913d0a54fb7c72a3b346281bdc5956eb1
edwin <edwin.abel.3@gmail.com>
2022-12-10 14:53:01 +0800

    parallel update.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 mod pig_latin;
+use rayon::prelude::*;
 use std::env;
 
 fn main() {
     let mut args: Vec<String> = env::args().collect();
     args.remove(0);
-    args.iter()
+    let output: Vec<String> = args
+        .par_iter()
         .map(|f| pig_latin::to_pig_latin(f))
-        .for_each(|f| print!("{} ", f));
+        .collect();
+    output.iter().for_each(|f| print!("{} ", f));
     println!();
 }
